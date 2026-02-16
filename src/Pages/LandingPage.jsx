9b0e046d7cf55fc994f703ab35d91b7f1e50d41f
nili <145498352+niloofar-shahsavar@users.noreply.github.com>
2026-02-16 16:09:18 +0100

    Improved style

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -5,7 +5,7 @@ import "../Style/Navbar.css";
 import "../Style/LandingPage.css";
 import cvImage from "../assets/CV_Niloo.jpg";
 import cvPdf from "../assets/CV_Niloo.pdf";
-import Subjectc from "../assets/Linkdin-photo.jpg";
+import Subjectc from "../assets/hero-img.jpg";
 import project1 from "../assets/luxbite-landing.jpg";
 import RandomStuff from "../assets/randomstuff-landing.png";
 import arrowIcon from "../assets/Arrow.svg";
